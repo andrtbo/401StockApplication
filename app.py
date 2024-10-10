0338f46aa78d2302d06ef6fe528dc17e1aad1efd
andrtbo <aboynes@asu.edu>
2024-10-10 00:26:55 -0700

    Login / Create Account pages
    Created a form class for user information.
    
    Created a card class on the login and create account pages that use the user form.
    
    Currently doesn't send the information anywhere since database isn't set up.

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FloatField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'temp_key' #Change key later
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 app.config['WTF_CSRF_ENABLED'] = False
+
+# Classes for database and forms
+class UserForm(FlaskForm): #Form with fields required for logging in
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
 
 @app.route("/")
 def dashboard():
@@ -32,4 +38,12 @@ def portfolio():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    form = UserForm()
+
+    return render_template('login.html', form=form)
+
+@app.route("/create-account")
+def create_account():
+    form = UserForm()
+    
+    return render_template('create_account.html', form=form)
