e650d94126afb2fb72fed45ffe3afb2c6d717e34
andrtbo <aboynes@asu.edu>
2024-10-10 13:41:03 -0700

    Created database tables
    ERD tables have been put into app.py and work with MySQL. Might still need modification once we start working on things though.

--- app.py
+++ app.py
@@ -11,7 +11,38 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.config['WTF_CSRF_ENABLED'] = False
 
-# Classes for database and forms
+# Classes for databases
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique = True, nullable = False)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    password_hash = db.Column(db.String(255))
+    admin = db.Column(db.Boolean)
+
+class Stock(db.Model):
+    stock_ticker = db.Column(db.String(5), primary_key=True)
+    company_name = db.Column(db.String(255))
+    market_price = db.Column(db.Float, nullable = False)
+    volume_owned = db.Column(db.Integer)
+    market_volume = db.Column(db.Integer)
+
+class Portfolio(db.Model):
+    portfolio_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    stock_ticker = db.Column(db.String(5), db.ForeignKey(Stock.stock_ticker))
+    total_balance = db.Column(db.Float)
+    cash_balance = db.Column(db.Float)  
+
+class Transactions(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    stock_ticker = db.Column(db.String(5), db.ForeignKey(Stock.stock_ticker))
+    purchase_price = db.Column(db.Float)
+    purchase_volume = db.Column(db.Integer)
+
+# Classes for forms
 class UserForm(FlaskForm): #Form with fields required for logging in
     username = StringField('Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
@@ -22,6 +53,7 @@ class UserForm(FlaskForm): #Form with fields required for logging in
 
 @app.route("/")
 def dashboard():
+    db.create_all()
     return render_template('dashboard.html')
 
 @app.route("/buy_stock") 
