22392471bbcf83bf0f6f16f4f3ae416a1ee2d1c7
andrtbo <aboynes@asu.edu>
2024-11-13 18:35:14 -0700

    Fixed database
    Forgot I switched to a secondary database for testing

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from .models import User, MarketHours, LastUpdated
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'temp_key' #Change key later
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/flask_test'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/flask'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['WTF_CSRF_ENABLED'] = False
     
